cc5d3a53adf8584cddf4a3c348ea03eab4f5f30d
Malody Hoe <malodyhoe@gmail.com>
2014-12-06 16:17:53 +0800

    Initial commit

---
+++ Assets/Scripts/Arena.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class Arena : MonoBehaviour
+{
+
+	public Transform FloorPrefab;
+	public Transform WallPrefab;
+
+	public int Rows = 10;
+	public int Columns = 10;
+
+	private Transform transformCache;
+	private List<Transform> walls;
+
+	public void Expand()
+	{
+		Rows += 2;
+		Columns += 2;
+
+		CreateWalls();
+		Camera.main.orthographicSize = Mathf.Min(Rows, Columns) / 2;
+	}
+
+	private void CreateWalls()
+	{
+		foreach (Transform wall in walls)
+		{
+			Destroy(wall.gameObject);
+		}
+		walls.Clear();
+		
+		for (int row = 0; row < Rows; row++)
+		{
+			if (row == 0 || row == Rows - 1)
+			{
+				for (int col = 1; col < Columns - 1; col++)
+				{
+					CreateWallAt(row, col);
+				}
+			}
+			CreateWallAt(row, 0);
+			CreateWallAt(row, Columns - 1);
+		}
+	}
+
+	private void CreateWallAt(int row, int column)
+	{
+		Transform wall = Instantiate(WallPrefab,
+			new Vector3(column - Columns / 2 + 0.5f, row - Rows / 2 + 0.5f), Quaternion.identity) as Transform;
+		wall.parent = transformCache;
+		walls.Add(wall);
+	}
+
+	void Awake()
+	{
+		transformCache = transform;
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+		walls = new List<Transform>();
+		CreateWalls();
+
+		Camera.main.orthographicSize = Mathf.Min(Rows, Columns) / 2;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+	public float Acceleration = 1.0f;
+	public float MaxVelocity = 1.0f;
+
+	private Rigidbody2D rigidbodyCache;
+
+	private int horizontal;
+	private int vertical;
+
+	void Awake()
+	{
+		rigidbodyCache = rigidbody2D;
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		horizontal = (int) Input.GetAxisRaw("Horizontal");
+		vertical = (int) Input.GetAxisRaw("Vertical");
+
+	}
+
+	void FixedUpdate()
+	{
+		if (horizontal == 0 && vertical == 0)
+			rigidbodyCache.velocity =
+				Vector2.MoveTowards(rigidbodyCache.velocity, Vector2.zero, Acceleration * Time.fixedDeltaTime);
+		rigidbodyCache.velocity += Vector2.ClampMagnitude(
+			new Vector2(horizontal, vertical) * Acceleration, Acceleration) * Time.fixedDeltaTime;
+		rigidbodyCache.velocity = Vector2.ClampMagnitude(rigidbodyCache.velocity, MaxVelocity);
+	}
+
+}
